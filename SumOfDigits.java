82124a604769bda25c4daad421c2b1caf9bb1111
dynamicdev4all <149931467+dynamicdev4all@users.noreply.github.com>
2025-09-24 10:56:41 +0530

    Add files via upload
    This is the code for Java MCA Batch

---
+++ SumOfDigits.java
@@ -0,0 +1,20 @@
+public class SumOfDigits {
+
+    int findSum(int num) {
+        if (num <= 0) {
+            return 0;
+        }
+
+        return (num % 10) + findSum(num / 10);
+    }
+
+    public static void main(String[] args) {
+        int num = 8459090;
+        int sum = 0;
+        while (num != 0) {
+            int last = num % 10;
+            sum = sum + last;
+            num = num / 10;
+        }
+    }
+}
--- functions/RecursionDemo.java
+++ functions/RecursionDemo.java
@@ -1,6 +1,10 @@
 package functions;
 
 public class RecursionDemo {
+    public static void main(String[] args) {
+        RecursionDemo rd = new RecursionDemo(); // object
+        rd.printData2(5);
+    }
 
     void printData2(int count) {
         // this is the base case, where recursion ends
@@ -14,8 +18,4 @@ public class RecursionDemo {
 
     }
 
-    public static void main(String[] args) {
-        RecursionDemo rd = new RecursionDemo(); // object
-        rd.printData2(5);
-    }
 }
