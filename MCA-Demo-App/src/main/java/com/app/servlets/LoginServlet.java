0e38324266b01bb3fbbb1e0cf45bbfeae9d337da
dynamicdev4all <149931467+dynamicdev4all@users.noreply.github.com>
2025-10-27 09:41:22 +0530

    Add files via upload

---
+++ MCA-Demo-App/src/main/java/com/app/servlets/LoginServlet.java
@@ -0,0 +1,50 @@
+package com.app.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("email_k");
+		String password = request.getParameter("password_k");
+		
+		System.out.println("Your email add is :"+email);
+		System.out.println("Your password is :"+password);
+		if(email.equals("admin@rdec.in") && password.equals("123456")) {
+			response.sendRedirect("home.html");
+		}else {
+			System.out.println("Your email or password is incorrect");
+		}
+		System.out.println("YES");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
